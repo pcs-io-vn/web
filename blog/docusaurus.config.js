f7e2e8f82ceb1fbb80edf7511551cc84e32dbbc4
Hồng Bảo Ngân <hongbaongan@gmail.com>
2025-11-07 08:17:40 +0700

    Update docusaurus.config.js
    'https://github.com/JaxVN/jaxvn-blog/edit/main/blog/'

--- blog/docusaurus.config.js
+++ blog/docusaurus.config.js
@@ -58,7 +58,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/JaxVN/jaxvn-blog/tree/main/blog/',
+            'https://github.com/JaxVN/jaxvn-blog/edit/main/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
